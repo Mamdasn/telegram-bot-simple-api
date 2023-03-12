a1c231461a6592de4fc467bc436f0a80e30041f3
Farhad <mhsaber1375@gmail.com>
2023-03-12 04:32:30 +0100

    Add a new api library

---
+++ xposedornot_api.py
@@ -0,0 +1,19 @@
+import requests
+from Crypto.Hash import keccak
+
+def check_password(password):
+    password_bytes = password.encode('utf-8') 
+    keccak_512 = keccak.new(digest_bits=512)
+    hash_object = keccak_512.update(password_bytes)
+    hex_dig = hash_object.hexdigest()
+    base_link = "https://passwords.xposedornot.com/api/v1/pass/anon"
+    with requests.Session() as session:
+        response = session.get(f"{base_link}/{hex_dig[:10]}")
+        message = response.json()
+        if message.get("SearchPassAnon"):
+            count = message.get('SearchPassAnon').get('count')
+            wordlist = message.get('SearchPassAnon').get('wordlist')
+            return count, wordlist 
+        else:
+            return None, None
+
